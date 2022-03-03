ba33587684954c18f23c7955e41eaf332c6936c1
Muresan Paul Mihai <paulmuresan1235@gmail.com>
2022-03-03 18:48:43 +0200

    first push

---
+++ src/main/java/com/utcn/sneakershop/SneakerShopApplication.java
@@ -0,0 +1,13 @@
+package com.utcn.sneakershop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SneakerShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SneakerShopApplication.class, args);
+    }
+
+}
