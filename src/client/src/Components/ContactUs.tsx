799882ab0ffbccc5352fd1c1a01ffc3471133b4f
mihut-andreea-ioana-30125 <andreeaandreea_2000@yahoo.com>
2022-04-01 17:24:10 +0300

    Navbar,Footer,Slideshow,Page for register and login

--- src/client/src/Components/ContactUs.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-function ContactUs() {
-
-  return(
-      <div></div>
-  )
-}
-
-export default ContactUs;
--- src/client/src/Components/Man.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-function Man() {
-
-  return(
-      <div>Products for men</div>
-  )
-}
-
-export default Man;
